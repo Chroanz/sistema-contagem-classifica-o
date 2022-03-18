6cc487321b90d10a03e64dbab15120b6e8a86012
Chroanz <christianhcoa@gmail.com>
2022-03-18 20:10:27 -0300

    primeiro commit

---
+++ script2.js
@@ -0,0 +1,10 @@
+let numeros = 20
+
+for(let contador = 0; contador <= numeros; contador = contador + 1)
+{
+ if(contador % 2 == 0){
+     console.log(contador+" é par");
+ }else{
+     console.log(contador+" é ímpar");
+ }
+}
